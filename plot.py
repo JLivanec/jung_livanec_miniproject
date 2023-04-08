b5d8535bc7e0db03e3ac490eb333389aa02f05ca
Jackson Livanec <jlivanec@vt.edu>
2023-04-07 22:29:15 -0400

    changed single param

--- plot.py
+++ plot.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from natural_selection import simulate
 
-agent_count, fitness = simulate(100)
+agent_count, fitness = simulate(100, 100)
 
 fig, axs = plt.subplots(2)
 fig.suptitle("Agent Population and Fitness Over Time")
